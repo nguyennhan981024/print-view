eaf9154dba5468227a6600a013d5c4a4c177ca10
NguyenMinhThang <thang.nguyenminh@emea.nttdata.com>
2023-02-21 11:43:25 +0700

    fix build erorr

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./Header";
 import InputDocumentModal from "./InputDocumentModal";
 import TableItem from "./TableItem";
